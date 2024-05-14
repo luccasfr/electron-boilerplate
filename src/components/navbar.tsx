1351811bf8ad33178eaf6f2ed886c207b84dd305
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-05-14 13:55:53 -0300

    Increase navbar gap.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 export default function Navbar() {
   return (
     <div className="fixed w-full flex justify-between items-center border-b px-4 py-2 bg-background/80 backdrop-blur">
-      <div className="flex gap-2">
+      <div className="flex gap-4 text-sm">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
